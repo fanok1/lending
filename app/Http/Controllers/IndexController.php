7b6ea1a384c75f0d2621f000501e1b432b2a9469
Alexandr Francuzevich <francuzevich98@gmail.com>
2018-03-30 11:13:42 +0300

    index view

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Page;
+use App\Service;
+use App\Portfolio;
+use App\People;
 
 class IndexController extends Controller
 {
     public function execute(Request $request){
 
+        $pages= Page::all();
+        $portfolios= Portfolio::get(["name", "images", "filter"]);
+        $services = Service::where("id","<",20)->get();
+        $peoples = People::take(3)->get();
 
-        return view("layouts.syte");
+
+        return view("syte.index");
     }
 }
---
+++ resources/views/syte/index.blade.php
@@ -0,0 +1,9 @@
+@extends("layouts.syte");
+
+@section("header")
+    @include("syte.header")
+@endsections
+
+@section("content")
+    @include("syte.content")
+@endsections
